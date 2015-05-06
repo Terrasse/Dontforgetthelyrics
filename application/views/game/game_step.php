6c45b232407637ff2ff721dd809cad4b74dbfed7
Choudoor <gauthierfreund@free.fr>
2015-05-06 22:05:57 +0200

    Url & local link We can use only internet

--- application/views/game/game_step.php
+++ application/views/game/game_step.php
@@ -2,8 +2,13 @@
 	echo $id_music.' '.$title.'<br />'.$lyrics;
 ?>
 
+<h5>Local link - <?php echo base_url(); ?>assets/musiques/<?php echo $id_music; ?>.mp3</h5>
 <audio id="player2" src="<?php echo base_url(); ?>assets/musiques/<?php echo $id_music; ?>.mp3" type="audio/mp3" controls="controls">		
-</audio>	
+</audio>
+
+<h5>Url - http://jplayer.org/audio/mp3/Miaow-07-Bubble.mp3</h5>
+<audio id="player2" src="http://jplayer.org/audio/mp3/Miaow-07-Bubble.mp3" type="audio/mp3" controls="controls">		
+</audio>
 
 <script>
 $('audio,video').mediaelementplayer();
