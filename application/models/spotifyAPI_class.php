28e19866fe879d688647440c37e71f4e2d5360fe
Terry DERVAUX <d.terry@orange.fr>
2015-05-08 22:48:06 +0200

    Add artists on Spotify API

--- application/models/spotifyAPI_class.php
+++ application/models/spotifyAPI_class.php
@@ -11,11 +11,16 @@ class SpotifyAPI_class extends CI_Model {
 	public function searchMusics($name_musique, $offset) {
 		$output = self::_callAPI('/search', array('q' => $name_musique, 'type' => "track", 'market' => "US", 'limit' => 50, 'offset' => $offset));
 		if ($output != 0) {
-			foreach ($output['tracks']['items'] as $key => $value) {
+			foreach ($output['tracks']['items'] as $key => $tracks_values) {
 				$current_tracks = $output['tracks']['items'][$key];
 				if ($current_tracks['explicit'] == TRUE) {
-					$musics[$current_tracks['name']] = array($current_tracks['uri'], $current_tracks['album']['name']);
+					$musics[$current_tracks['name']]["spotify_id"] = $current_tracks['uri'];
+					$musics[$current_tracks['name']]["album"] = $current_tracks['album']['name'];
+					foreach($current_tracks['artists'] as $key_artist => $artiste_values){
+						$musics[$current_tracks['name']]['artists'][$key_artist]= $artiste_values['name'];
+					}
 				}
+				
 			}
 			return $musics;
 		} else
